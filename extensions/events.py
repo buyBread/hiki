78b6ee66e07b7cbea5246da9d16b0f98307cc566
buyBread <buymybreadalt@gmail.com>
2020-08-15 01:04:53 +0300

    databse test

--- extensions/events.py
+++ extensions/events.py
@@ -1,13 +1,36 @@
+import sqlite3
 from discord.ext import commands
 
 class Events(commands.Cog):
     
     def __init__(self, bot):
         self.bot = bot
+        self.db = sqlite3.connect('database.db')
+        self.cs = self.db.cursor()
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print("ready")
+        print("on_ready")
+
+        self.cs.execute("CREATE TABLE IF NOT EXISTS users(id integer NOT NULL, messages integer NOT NULL)")
+        for member in self.bot.get_all_members():
+            if member.id == self.bot.user.id:
+                return
+
+            if len(self.cs.execute('SELECT * FROM users WHERE id=?', (member.id, )).fetchall()) == 0:
+                self.cs.execute('INSERT INTO users(id, messages) VALUES(?, ?)', (member.id, 0))
+                self.db.commit()
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        author = message.author
+
+        if author.id == self.bot.user.id:
+            return
+
+        self.cs.execute('UPDATE users SET messages=? + ? WHERE id=?', (self.cs.execute('SELECT messages FROM users WHERE id=?', (author.id, )).fetchall()[0][0], 1, author.id))
+        self.db.commit()
+        print("{0.name} now has {1} messages".format(author, self.cs.execute("SELECT messages FROM users WHERE id=?", (author.id, )).fetchall()[0][0]))
 
 def setup(bot):
     bot.add_cog(Events(bot))
--- main.py
+++ main.py
@@ -10,8 +10,10 @@ for extension in os.listdir('extensions'):
 extensions_list = []
 load_all_extensions = False
 
-print("Extensions to load (Default=all):", end = " ")
+print("Extensions to load (Default=all):")
+print(" => ", end="")
 extensions_to_load = input()
+print()
 
 if extensions_to_load == "" or "all":
     load_all_extensions = True
@@ -28,6 +30,13 @@ else:
         if extension in extensions_list:
             bot_client.load_extension(f"extensions.{extension[:-3]}")
 
-token = ""
+"""
+# for c+p in da future
+# im lazy lol
+
+self.db = sqlite3.connect('database.db')
+self.cs = self.db.cursor()
+"""
+
 # god bless
-bot_client.run(token)
+bot_client.run("")
